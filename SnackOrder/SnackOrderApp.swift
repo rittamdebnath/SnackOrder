2a0af31da838fac477f5b6f4e60f0966a7ca1b0a
rittamdebnath <rittamdebnath@gmail.com>
2024-06-30 16:04:40 +0530

    Initial Commit

---
+++ SnackOrder/SnackOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackOrderApp.swift
+//  SnackOrder
+//
+//  Created by Rittam Debnath on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SnackOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
